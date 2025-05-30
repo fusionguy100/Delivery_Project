f386cf21ff3d9b5b58b9c7e83c901815dfb48eb2
Jacob Newell <163172507+fusionguy100@users.noreply.github.com>
2025-05-30 13:33:27 -0700

    Initial Commit

---
+++ package.py
@@ -0,0 +1,10 @@
+class Package:
+    def __init__(self, package_id, address, city, state, zip_code, delivery_deadline, weight_kilo, special_notes):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.delivery_deadline = delivery_deadline
+        self.weight_kilo = weight_kilo
+        self.special_notes = special_notes
